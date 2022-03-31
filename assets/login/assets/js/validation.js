1ad200f25c362f8f147f74d31a6d2fbc6b2922b1
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-03-31 20:09:38 +0200

    login and signup using localStorage

--- assets/login/assets/js/validation.js
+++ assets/login/assets/js/validation.js
@@ -13,9 +13,10 @@ let email = id("email"),
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  
   validate(email, 0, "Email cannot be blank");
   validate(password, 1, "Password cannot be blank");
+  
 });
 
 // validate function which will do all the works
@@ -26,8 +27,52 @@ let validate = (inputField, serial, message) => {
     inputField.parentNode.style.border = "2px solid #af4444";
  
   } else {
+    
+    
     errorMsg[serial].innerHTML = "";
     inputField.parentNode.style.border = "2px solid green";
     
   }
 };
+
+// function Login() {
+  
+//   let users = [
+//     {
+//       name:"abcd@gmail.com",
+//       password:btoa("abc@12")
+//      },
+
+//      {
+//       name:"bcd@gmail.com",
+//       password:btoa("bcd@12")
+//      },
+//   ]
+
+//   let usernameValue = email.value;
+//   let passwordValue = password.value;
+
+//   sessionStorage.setItem("currentloggedin", usernameValue);
+
+//   localStorage.setItem('all_users',JSON.stringify(users));
+
+//   const hash = Object.fromEntries(
+//     a.map(e => [e.name, e.password])
+//   )
+
+//   for (let key of hash) {
+    
+//     if(key[0] === usernameValue && key[1]===atob(passwordValue)) {
+//          alert('Login successful');
+//      }
+ 
+//     else {
+//          alert("login fail");
+//      }
+//   }
+
+//   let hashmap = a.reduce(function(map, obj) {
+//     map[obj.key] = obj.val;
+//     return map;
+// }, {});
+// }
